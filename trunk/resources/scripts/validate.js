4bc9a544fb8b6a399493fe02f3a46e9054ba055b
jdreddaway@gmail.com <jdreddaway@gmail.com@a27bdac9-9880-9630-c8d4-ba94c46f413d>
2012-06-14 23:39:00 +0000

    Finished validation on register.php. Still need to add database functionality.

--- trunk/resources/scripts/validate.js
+++ trunk/resources/scripts/validate.js
@@ -14,7 +14,9 @@ function isMatch(input1, input2)
 	return input1 == input2;
 }
 
+//http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
 function isValidEmail(email) //returns true or false
 {
-	return true;
+	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 }
