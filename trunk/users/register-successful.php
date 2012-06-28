33d7bca9803b4d52ac24f2ba08505d355b977f20
jdreddaway@gmail.com <jdreddaway@gmail.com@a27bdac9-9880-9630-c8d4-ba94c46f413d>
2012-06-28 22:26:57 +0000

    Touched up on a few registration things.

--- trunk/users/register-successful.php
+++ trunk/users/register-successful.php
@@ -6,11 +6,18 @@
 <head>
 	<title>Login</title>
 	<?php gtInclude("includes/head.php"); ?>
+	<style type="text/css">
+		#content
+		{
+			padding:5px;
+			text-align:center;
+		}
+	</style>
 </head>
 <body>
 <div id="main">
 	<?php gtInclude("includes/top.php"); ?>
-	<p>
+	<p id="content" class="primaryFGColor">
 		Registration successful!
 	</p>
 	<?php gtInclude("includes/foot.php"); ?>
