a92d750afa50a024e402e3712cd80fe200463c37
jdreddaway@gmail.com <jdreddaway@gmail.com@a27bdac9-9880-9630-c8d4-ba94c46f413d>
2012-07-23 22:20:38 +0000

    Worked on roles.

--- trunk/resources/classes/basement/RolesRepository.class.php
+++ trunk/resources/classes/basement/RolesRepository.class.php
@@ -4,7 +4,23 @@ class RolesRepository
 {
 	public function retrieveRolesForUser($user)
 	{
-		//TODO finish
+		$connection = DBConnection::getConnection();
+		
+		$userID = $user->getID();
+		$statement = $connection->prepare("SELECT role_id FROM UserRoles WHERE user_id = :userID");
+		$statement->bindParam(":userID", $userID);
+		
+		$statement->execute();
+		
+		$roles = array();
+		$i = 0;
+		
+		while ($row = $statement->fetch())
+		{
+			$roles[$i++] = new Role($row[0]);
+		}
+		
+		return $roles;
 	}
 }
 
