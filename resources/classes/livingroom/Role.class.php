553fbfccde42c70ec94dbda582e9a66ac4b4d90e
jdreddaway <jdreddaway@gmail.com>
2012-07-23 22:20:38 +0000

    Worked on roles.

--- resources/classes/livingroom/Role.class.php
+++ resources/classes/livingroom/Role.class.php
@@ -9,6 +9,31 @@ class Role
 		$this->role = $role;
 	}
 	
+	/**
+		returns true if $user has this role
+	*/
+	public function userHasRole($user)
+	{
+		$userRoles = RolesRepository::retrieveRolesForUser($user);
+		
+		$i = 0;
+		while($i < count($userRoles))
+		{
+			if ($this->equals($userRoles[$i]))
+			{
+				return true;
+			}
+			$i++;
+		}
+		
+		return false;
+	}
+	
+	public function equals($other)
+	{
+		return $this->role == $other->role;
+	}
+	
 	public static function getRole($role)
 	{
 		return new Role($role);
