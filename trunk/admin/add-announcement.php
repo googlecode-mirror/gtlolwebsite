51d7ca862e4696a7cbfc13b2dd25820338a77a62
jdreddaway@gmail.com <jdreddaway@gmail.com@a27bdac9-9880-9630-c8d4-ba94c46f413d>
2012-07-30 22:41:16 +0000

    Worked on announcements that administrators can make. Added announcements table to database.

--- trunk/admin/add-announcement.php
+++ trunk/admin/add-announcement.php
@@ -1,4 +1,7 @@
-<?php require $_SERVER['DOCUMENT_ROOT'] . "/resources/scripts/initialize.php"; ?>
+<?php
+	require $_SERVER['DOCUMENT_ROOT'] . "/resources/scripts/initialize.php";
+	gtRequireOnce("scripts/requireAdmin.php");
+?>
 
 <!DOCTYPE html>
 
@@ -11,7 +14,12 @@
 <div id="main">
 	<?php gtInclude("includes/top.php"); ?>
 	<div class="primaryFGColor contentBox">
-		
+		<h2>Add Announcement</h2>
+		<form id="frmAnnouncement" name="frmAnnouncement" method="post">
+			<!-- TODO: make this a table -->
+			Title: <input id="title" name="title" type="text" size="100" maxlength="255" autofocus="autofocus" />
+			Announcement: <textarea id="announcement" name="announcement" cols="100" rows="4"></textarea>
+		</form>
 	</div>
 	<?php gtInclude("includes/foot.php"); ?>
 </div>
