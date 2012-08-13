2a2d7c46183587524d871dd0cd035328fa1cd37c
jdreddaway@gmail.com <jdreddaway@gmail.com@a27bdac9-9880-9630-c8d4-ba94c46f413d>
2012-08-13 22:44:32 +0000

    Worked on announcements. Added executeSQLSelect() and executeSQLCommand() to DBConnection to make executing queries easier. Still need to refactor repositories to use these changes.

---
+++ trunk/resources/classes/controllers/AnnouncementController.class.php
@@ -0,0 +1,26 @@
+<?php
+
+class AnnouncementController
+{
+	/**
+		returns an array of arrays that represent announcements.
+			Each "announcement" has the following key/value pairs:
+				title : String
+				text : String
+				time : DateTime
+				announcer_username : String
+	*/
+	public static function getTopNLatestAnnouncements($n)
+	{
+		$announcements = AnnouncementsRepository::retrieveAnnouncements(null, $n);
+		
+		//TODO finish
+	}
+	
+	private static function convertAnnouncementToArray($announcement)
+	{
+		//TODO finish
+	}
+}
+
+?>
